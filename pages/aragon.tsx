eb67a8799ee93e973611957341eca7585fd6eff4
Aaron Foster <50079365+pythonpete32@users.noreply.github.com>
2022-09-24 13:42:56 +0400

    feat: add Aragon

---
+++ pages/aragon.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CreateDaoCard } from '../components/CreateDaoCard'
+
+const aragon = () => {
+  return (
+    <div className="flex align-middle justify-center h-screen bg-base-200">
+      <div className="py-20">
+        <CreateDaoCard />
+      </div>
+    </div>
+  )
+}
+
+export default aragon
